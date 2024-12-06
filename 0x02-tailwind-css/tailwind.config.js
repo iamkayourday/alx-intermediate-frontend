0a4120152b893332559213dbca11380fe2f67e92
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2024-12-06 10:16:25 +0100

    Finally figured out how to run tailwind with html

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./**/*.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
